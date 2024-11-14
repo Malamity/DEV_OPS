fc3c452b557d11fb87335006caf6f9e9593a1f44
michalszura <115370163+Malamity@users.noreply.github.com>
2024-11-14 21:03:10 +0100

    Added weather modules, changed css folder name, added weather api, made few minor changes

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from weather import get_current_weather
+from waitress import serve
 
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
-def hello_name(name):
-    return 'Hello %s!' %name
-
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city')
+    weather_data = get_current_weather(city)
+    return render_template(
+        "weather.html",
+        title=weather_data['name'],
+        status=weather_data['weather'][0]['description'].capitalize(),
+        temp=f"{weather_data['main']['temp']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}"
+    )
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    serve(app, host='0.0.0.0', port=8000)
+
